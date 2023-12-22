5f299e1f39570c9fa9b3cdc5a9eaaa17ba9358a1
pandupan <pangestup60@gmail.com>
2023-12-22 08:26:48 +0700

    [ADD] - API ROUTES

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,16 @@ type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  // res.status(200).json({ name: 'John Doe' }) 
+  try {
+    const response = await (await fetch("https://dummyjson.com/users")).json();
+    res.status(200).json({... response });
+  } catch (error) {
+
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,17 @@ import Head from "next/head";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Layout from "@/Layout"
+import { useEffect } from "react";
 
 export default function Main({children} : any) {
+  
+  useEffect(() => {
+    fetch("/api/hello")
+    .then(res => res.json())
+      .then((res) => console.log("response =>", res))
+      .catch((err) => console.log("err =>", err));
+  }, []);
+
  return (
   <>
     <Layout metaTitle="Home" metaDescription={"Ini adalah bagian Home"}>
