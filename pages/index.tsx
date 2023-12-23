3889490be208d46d046e82964955ec66097e471c
pandupan <pangestup60@gmail.com>
2023-12-23 08:29:17 +0700

    [ADD] Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Layout from "@/Layout"
 import { useEffect } from "react";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+
+const LayoutComponent = dynamic(() => import("@/Layout"));
 
 export default function Main({children} : any) {
   
@@ -15,9 +19,15 @@ export default function Main({children} : any) {
 
  return (
   <>
-    <Layout metaTitle="Home" metaDescription={"Ini adalah bagian Home"}>
-        <p className="w-full min-h-[100vh]">Home</p>
-    </Layout>
+    <LayoutComponent metaTitle="Home" metaDescription={"Ini adalah bagian Home"}>
+        <div className="flex flex-row">
+          <p className="text-6xl p-32">Home</p>
+          <Image
+            src="/next.png" width={400} height={400} 
+            alt="next image" className="object-cover"
+          />
+        </div>
+    </LayoutComponent>
   </>
  );
 }
